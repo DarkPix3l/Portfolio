4251a06e9f496aafd4d7f7863a737707ff495408
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2025-08-16 18:09:28 +0200

    feat(loader):  made page loader self-contained via JS injection:
    - Loader HTML is now injected dynamically into the page
    
    - The insertAdjacentHTML method allows inserting HTML content without overwriting or altering existing nodes
    
    - After the transition has finished, the html is completely unmounted

--- js/pageLoader.js
+++ js/pageLoader.js
@@ -1,15 +1,18 @@
+const loaderHTML = `
+<div id="loaderPage">
+<div class="loader"></div>
+</div>
+`;
+document.body.insertAdjacentHTML('afterbegin', loaderHTML);
 const loaderPage = document.getElementById("loaderPage");
 
-/* window.addEventListener("DOMContentLoaded", () => {
-  loaderPage.style.opacity = "1";
-}); */
 
 window.addEventListener("load", () => {
   loaderPage.style.opacity = "0";
   loaderPage.style.pointerEvents = "none";
-}); 
+
+});
 
 loaderPage.addEventListener("transitionend", () => {
-  loaderPage.style.display = "none";
-  loaderPage.style.pointerEvents = "auto";
+  loaderPage.remove();
 });
