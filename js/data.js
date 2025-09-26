a388823bae5dd33480535f572c149ee8ba9669e5
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2025-09-26 16:12:21 +0200

    Add paths for project 4 in `data.js` and `index.html`. Update paths and sizes for project 1 in `data.js` to match horizontal images.

--- js/data.js
+++ js/data.js
@@ -1,4 +1,5 @@
 const sizes = `
+(max-width: 3840px) 3840px,
 (max-width: 2560px) 2560px,
 (max-width: 1920px) 1920px,
 (max-width: 1440px) 1280px,
@@ -20,13 +21,14 @@ const projects = [
     mainImage: {
       src: "../assets/img/projects/project1/mainImage/0_elevher_preview_ver.576.webp",
       srcset: `
-        ../assets/img/projects/project1/1_mainImage/0_elevher_preview_ver.576.webp 320w,
-        ../assets/img/projects/project1/1_mainImage/0_elevher_preview_ver.576.webp 375w, 
-        ../assets/img/projects/project1/1_mainImage/0_elevher_preview_ver.768.webp 768w,
-        ../assets/img/projects/project1/1_mainImage/0_elevher_preview_ver.2250.webp 1024w,
-        ../assets/img/projects/project1/1_mainImage/0_elevher_preview_ver.2250.webp 1440w,
-        ../assets/img/projects/project1/1_mainImage/0_elevher_preview_ver.3240.webp 1920w,
-        ../assets/img/projects/project1/1_mainImage/0_elevher_preview_ver.3240.webp 2560w
+        ../assets/img/projects/project1/1_mainImage/1_elevHer_main_320.webp 320w,
+        ../assets/img/projects/project1/1_mainImage/1_elevHer_main_375.webp 375w, 
+        ../assets/img/projects/project1/1_mainImage/1_elevHer_main_768.webp 768w,
+        ../assets/img/projects/project1/1_mainImage/1_elevHer_main_1024.webp 1024w,
+        ../assets/img/projects/project1/1_mainImage/1_elevHer_main_1440.webp 1440w,
+        ../assets/img/projects/project1/1_mainImage/1_elevHer_main_1920.webp 1920w,
+        ../assets/img/projects/project1/1_mainImage/1_elevHer_main_2560.webp 2560w,
+        ../assets/img/projects/project1/1_mainImage/1_elevHer_main_4000.webp 3840w
         `,
       sizes: sizes,
     },
@@ -83,7 +85,7 @@ const projects = [
         caption: "Products section",
       },
       {
-        src: "../assets/img/projects/project1/6_elevHer_color_palette/4_elevHer_productCard_showcase_320.webp",
+        src: "../assets/img/projects/project1/6_elevHer_color_palette/6_elevHer_color_palette_320.webp",
         srcset: `
         ../assets/img/projects/project1/6_elevHer_color_palette/6_elevHer_color_palette_320.webp 320w,
         ../assets/img/projects/project1/6_elevHer_color_palette/6_elevHer_color_palette_375.webp 375w,
@@ -109,14 +111,14 @@ const projects = [
         caption: "Materials - qualities",
       },
       {
-        src: "../assets/img/projects/project1/8_elevHer_whyCooseUs_mockup/8_elevHer_whyCooseUs_mockup_320.webp",
+        src: "../assets/img/projects/project1/8_elevHer_whyChooseUs_mockup/8_elevHer_whyCooseUs_mockup_320.webp",
         srcset: `
-        ../assets/img/projects/project1/8_elevHer_whyCooseUs_mockup/8_elevHer_whyCooseUs_mockup_320.webp 320w,
-        ../assets/img/projects/project1/8_elevHer_whyCooseUs_mockup/8_elevHer_whyCooseUs_mockup_375.webp 375w,
-        ../assets/img/projects/project1/8_elevHer_whyCooseUs_mockup/8_elevHer_whyCooseUs_mockup_768.webp 768w,
-        ../assets/img/projects/project1/8_elevHer_whyCooseUs_mockup/8_elevHer_whyCooseUs_mockup_1024.webp 1024w,
-        ../assets/img/projects/project1/8_elevHer_whyCooseUs_mockup/8_elevHer_whyCooseUs_mockup_1440.webp 1440w,
-        ../assets/img/projects/project1/8_elevHer_whyCooseUs_mockup/8_elevHer_whyCooseUs_mockup_1920.webp 1920w
+        ../assets/img/projects/project1/8_elevHer_whyChooseUs_mockup/8_elevHer_whyCooseUs_mockup_320.webp 320w,
+        ../assets/img/projects/project1/8_elevHer_whyChooseUs_mockup/8_elevHer_whyCooseUs_mockup_375.webp 375w,
+        ../assets/img/projects/project1/8_elevHer_whyChooseUs_mockup/8_elevHer_whyCooseUs_mockup_768.webp 768w,
+        ../assets/img/projects/project1/8_elevHer_whyChooseUs_mockup/8_elevHer_whyCooseUs_mockup_1024.webp 1024w,
+        ../assets/img/projects/project1/8_elevHer_whyChooseUs_mockup/8_elevHer_whyCooseUs_mockup_1440.webp 1440w,
+        ../assets/img/projects/project1/8_elevHer_whyChooseUs_mockup/8_elevHer_whyCooseUs_mockup_1920.webp 1920w
         `,
         sizes: sizes,
         caption: "Desktop Mockup - Photo credit: Linus Mimietz on Unsplash",
@@ -137,18 +139,32 @@ const projects = [
     ],
     skills: [
       "UI & FRONTEND: Shadcn, Lucide Icons, Next.js, Tailwind CSS, React Hook Form, Zod, Zustand",
-      "BACKEND & AUTH: Express (ESM, modular routes/controllers), Auth.js, OAUTH, CORS, Bycrypt, Helmet, Mongoose, Joi",
-      "DATABASE: MongoDB"
+      "BACKEND & AUTH: Express (ESM, modular routes/controllers), Auth.js, OAUTH, CORS, Bcrypt, Helmet, Mongoose, Joi",
+      "DATABASE: MongoDB",
     ],
   },
   {
     pageId: "project2",
-    title: "Cool Project 2",
+    title: "Grow Blog",
     summary:
-      "Ut ac fringilla tellus. Proin quam metus, interdum ut posuere non, consequat eu ante. Etiam vel placerat ante. Donec mollis turpis sit amet ipsum commodo fermentum. Suspendisse feugiat augue ut massa gravida, pretium ultrices velit accumsan. Donec malesuada semper viverra.",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et lacus ac leoultrices posuere. Donec mattis tellus eu turpis porta rutrum. Nam lacus lorem, laoreet ut nibh non, vehicula egestas ante. Cras vulputate tristique quam, id fermentum dolor malesuada in. Praesent sed ullamcorper dolor. Nunc in accumsan nunc. Donec tempor dolor nec diam malesuada, sit amet feugiat lorem tempor. Vivamus a purus et magna semper fringilla commodo a urna. Donec sem ante, iaculis ut turpis eleifend, euismod molestie metus. Phasellus tincidunt dignissim lacus, porta sagittis mi fringilla sit amet. Nullam sit amet nisl sed est sagittis sagittis quis eu turpis. In porttitor mollis quam in interdum. In aliquam velit turpis, sed ultricies massa aliquet sed. Aliquam ac erat at nisl hendrerit lacinia vel id augue. Ut sed nisl at ipsum pharetra sollicitudin vel id elit.",
-    mainImage: "../assets/img/projects/beach.jpg",
+      "Grow-Blog is a statically generated blog built with Next.js and TypeScript that uses Markdown files (/_posts) to populate content. It emphasizes clean styling, neomorphic design touches, and supports automatic page generation for new posts via the file system.",
+    description: `
+      Grow-Blog is a modern blogging platform that leverages Next.js’s static generation features to turn Markdown files into full-fledged blog posts, each with its own page. 
+      Written in TypeScript for type safety, the project incorporates Sass/neomorphic CSS styles to deliver a visually appealing and responsive user interface. Every time a new Markdown file is added to the _posts directory, Grow-Blog automatically builds a new blog post page using the same layout but tailored content. The code structure emphasizes simplicity and maintainability, with clean file-based routing and styling that balances aesthetic polish with performance.
+      `,
+    mainImage: {
+      src: "../assets/img/projects/project2/mainImage/0_elevher_preview_ver.576.webp",
+      srcset: `
+        ../assets/img/projects/project2/mainImage/0_growBlog_preview_576.webp 320w,
+        ../assets/img/projects/project2/mainImage/0_growBlog_preview_576.webp 375w, 
+        ../assets/img/projects/project2/mainImage/0_growBlog_preview_768.webp 768w,
+        ../assets/img/projects/project2/mainImage/0_growBlog_preview_2250.webp 1024w,
+        ../assets/img/projects/project2/mainImage/0_growBlog_preview_2250.webp 1440w,
+        ../assets/img/projects/project2/mainImage/0_growBlog_preview_3240.webp 1920w,
+        ../assets/img/projects/project2/mainImage/0_growBlog_preview_3240.webp 2560w
+        `,
+      sizes: sizes,
+    },
 
     images: [
       {
@@ -180,7 +196,11 @@ const projects = [
         caption: "Admin dashboard interface",
       },
     ],
-    skills: ["UX Research", "HTML", "SCSS", "JS Animations"],
+    skills: [
+      "FRONTEND: Next.js, TypeScript, Sass, Responsive Layouts",
+      "CONTENT & DATA: Node.js Fs, Static Pages Generation, Markdown Parsing",
+      "STORAGE: Local file system",
+    ],
   },
   {
     pageId: "project3",
@@ -225,43 +245,71 @@ const projects = [
 
   {
     pageId: "project4",
-    title: "Neat Project 4",
+    title: "Easybank Landing Page",
     summary:
-      "Ut ac fringilla tellus. Proin quam metus, interdum ut posuere non, consequat eu ante. Etiam vel placerat ante. Donec mollis turpis sit amet ipsum commodo fermentum. Suspendisse feugiat augue ut massa gravida, pretium ultrices velit accumsan. Donec malesuada semper viverra.",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et lacus ac leoultrices posuere. Donec mattis tellus eu turpis porta rutrum. Nam lacus lorem, laoreet ut nibh non, vehicula egestas ante. Cras vulputate tristique quam, id fermentum dolor malesuada in. Praesent sed ullamcorper dolor. Nunc in accumsan nunc. Donec tempor dolor nec diam malesuada, sit amet feugiat lorem tempor. Vivamus a purus et magna semper fringilla commodo a urna. Donec sem ante, iaculis ut turpis eleifend, euismod molestie metus. Phasellus tincidunt dignissim lacus, porta sagittis mi fringilla sit amet. Nullam sit amet nisl sed est sagittis sagittis quis eu turpis. In porttitor mollis quam in interdum. In aliquam velit turpis, sed ultricies massa aliquet sed. Aliquam ac erat at nisl hendrerit lacinia vel id augue. Ut sed nisl at ipsum pharetra sollicitudin vel id elit.",
-    mainImage: "../assets/img/projects/station.jpg",
+      "A responsive landing page challenge from Frontend Mentor, built first with HTML, CSS & JavaScript, then refactored with React + TailwindCSS for a cleaner, component-based design. Focused on responsiveness, interactivity, and semantic structure.",
+    description: `This project recreates the Easybank landing page design with a mobile-first approach. The initial version was built using semantic HTML, CSS (Flexbox & Grid), and vanilla JavaScript for menu animations. Later, the project was refactored into a React + TailwindCSS implementation, improving maintainability and responsiveness while modernizing the workflow. Accessibility best practices (semantic markup, focusable elements) and basic performance improvements (lazy loading images, font preloading) were applied to enhance the overall user experience.
+      `,
+    mainImage: {
+      src: "../assets/img/projects/project4/1_mainImage/1_easybank_mainimage_320.webp",
+      srcset: `
+        ../assets/img/projects/project4/1_mainImage/1_easybank_mainimage_320.webp 320w,
+        ../assets/img/projects/project4/1_mainImage/1_easybank_mainimage_375.webp 375w,
+        ../assets/img/projects/project4/1_mainImage/1_easybank_mainimage_768.webp 768w,
+        ../assets/img/projects/project4/1_mainImage/1_easybank_mainimage_1024.webp 1024w,
+        ../assets/img/projects/project4/1_mainImage/1_easybank_mainimage_1440.webp 1440w,
+        ../assets/img/projects/project4/1_mainImage/1_easybank_mainimage_1920.webp 1920w,
+        ../assets/img/projects/project4/1_mainImage/1_easybank_mainimage_2560.webp 2560w,
+        ../assets/img/projects/project4/1_mainImage/1_easybank_mainimage_3841.webp 3840w
+        `,
+      sizes: sizes,
+    },
     images: [
       {
-        src: "../assets/img/placeholders/imgPlaceholder_hor_320.png",
-        srcset:
-          "../assets/img/placeholders/imgPlaceholder_hor_320.png 320w, ../assets/img/placeholders/imgPlaceholder_hor_375.png 375w, ../assets/img/placeholders/imgPlaceholder_hor_425.png 425w, ../assets/img/placeholders/imgPlaceholder_hor_768.png 768w, ../assets/img/placeholders/imgPlaceholder_hor_1024.png 1024w, ../assets/img/placeholders/imgPlaceholder_hor_1280.png 1280w, ../assets/img/placeholders/imgPlaceholder_hor_1440.png 1440w",
+        src: `../assets/img/projects/project4/2_desktop-showcase/1_easybank_desktop_showcase_320.webp`,
+        srcset: `
+        ../assets/img/projects/project4/2_desktop-showcase/2_easybank_desktop_showcase_320.webp 320w,
+        ../assets/img/projects/project4/2_desktop-showcase/2_easybank_desktop_showcase_375.webp 375w,
+        ../assets/img/projects/project4/2_desktop-showcase/2_easybank_desktop_showcase_768.webp 768w,
+        ../assets/img/projects/project4/2_desktop-showcase/2_easybank_desktop_showcase_1024.webp 1024w,
+        ../assets/img/projects/project4/2_desktop-showcase/2_easybank_desktop_showcase_1440.webp 1440w,
+        ../assets/img/projects/project4/2_desktop-showcase/2_easybank_desktop_showcase_1920.webp 1920w
+        `,
         sizes: sizes,
-        caption: "User flow for the onboarding",
+        caption: "Showcase Preview",
       },
       {
-        src: "../assets/img/placeholders/imgPlaceholder_hor_320.png",
-        srcset:
-          "../assets/img/placeholders/imgPlaceholder_hor_320.png 320w, ../assets/img/placeholders/imgPlaceholder_hor_375.png 375w, ../assets/img/placeholders/imgPlaceholder_hor_425.png 425w, ../assets/img/placeholders/imgPlaceholder_hor_768.png 768w, ../assets/img/placeholders/imgPlaceholder_hor_1024.png 1024w, ../assets/img/placeholders/imgPlaceholder_hor_1280.png 1280w, ../assets/img/placeholders/imgPlaceholder_hor_1440.png 1440w",
+        src: "../assets/img/projects/project4/3_active-states/3_easybank_active-states_320.webp",
+        srcset: `
+        ../assets/img/projects/project4/3_active-states/3_easybank_active-states_320.webp 320w,
+        ../assets/img/projects/project4/3_active-states/3_easybank_active-states_375.webp 375w,
+        ../assets/img/projects/project4/3_active-states/3_easybank_active-states_768.webp 768w,
+        ../assets/img/projects/project4/3_active-states/3_easybank_active-states_1024.webp 1024w,
+        ../assets/img/projects/project4/3_active-states/3_easybank_active-states_1440.webp 1440w,
+        ../assets/img/projects/project4/3_active-states/3_easybank_active-states_1920.webp 1920w
+        `,
         sizes: sizes,
-        caption: "Mobile responsive design",
+        caption: `Desktop Landing Page Design By Frontend Mentor`,
       },
       {
-        src: "../assets/img/placeholders/imgPlaceholder_hor_320.png",
-        srcset:
-          "../assets/img/placeholders/imgPlaceholder_hor_320.png 320w, ../assets/img/placeholders/imgPlaceholder_hor_375.png 375w, ../assets/img/placeholders/imgPlaceholder_hor_425.png 425w, ../assets/img/placeholders/imgPlaceholder_hor_768.png 768w, ../assets/img/placeholders/imgPlaceholder_hor_1024.png 1024w, ../assets/img/placeholders/imgPlaceholder_hor_1280.png 1280w, ../assets/img/placeholders/imgPlaceholder_hor_1440.png 1440w",
+        src: "../assets/img/projects/project4/4_mobile-design/4_easybank_mobile-design_320.webp",
+        srcset: `
+        ../assets/img/projects/project4/4_mobile-design/4_easybank_mobile-design_320.webp 320w,
+        ../assets/img/projects/project4/4_mobile-design/4_easybank_mobile-design_375.webp 375w,
+        ../assets/img/projects/project4/4_mobile-design/4_easybank_mobile-design_768.webp 768w,
+        ../assets/img/projects/project4/4_mobile-design/4_easybank_mobile-design_1024.webp 1024w,
+        ../assets/img/projects/project4/4_mobile-design/4_easybank_mobile-design_1440.webp 1440w,
+        ../assets/img/projects/project4/4_mobile-design/4_easybank_mobile-design_1920.webp 1920w
+        `,
         sizes: sizes,
-        caption: "Admin dashboard interface",
-      },
-      {
-        src: "../assets/img/placeholders/imgPlaceholder_hor_320.png",
-        srcset:
-          "../assets/img/placeholders/imgPlaceholder_hor_320.png 320w, ../assets/img/placeholders/imgPlaceholder_hor_375.png 375w, ../assets/img/placeholders/imgPlaceholder_hor_425.png 425w, ../assets/img/placeholders/imgPlaceholder_hor_768.png 768w, ../assets/img/placeholders/imgPlaceholder_hor_1024.png 1024w, ../assets/img/placeholders/imgPlaceholder_hor_1280.png 1280w, ../assets/img/placeholders/imgPlaceholder_hor_1440.png 1440w",
-        sizes: sizes,
-        caption: "Admin dashboard interface",
+        caption: "Easybank mobile layout with open menu – showcase preview",
       },
     ],
-    skills: ["Next.js", "Tailwind", "REST API", "GraphQL"],
+    skills: [
+      "FRONTEND: HTML, CSS, Flexbox, Grid, Tailwind CSS, React",
+      "INTERACTIVITY: Mobile menu animation, CSS hover states",
+      "ACCESSIBILITY & PERFORMANCE: Semantic HTML, Keyboard navigation awareness, Lazy loading, Font preloading",
+    ],
   },
 ];
 
